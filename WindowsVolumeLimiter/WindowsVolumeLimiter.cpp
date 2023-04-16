5ad58d2144b12794b0cb2382f5f14c283c1c0ca6
yutinglia <yutinglia@gmail.com>
2023-04-16 20:38:00 +0800

    rename some function and variable

--- WindowsVolumeLimiter/WindowsVolumeLimiter.cpp
+++ WindowsVolumeLimiter/WindowsVolumeLimiter.cpp
@@ -18,7 +18,7 @@
 #include <thread>
 
 
-std::wstring GetProcessNameFromPid(DWORD pid) {
+std::wstring getProcessNameFromPid(DWORD pid) {
 	HANDLE handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
 
 	if (handle) {
@@ -33,31 +33,35 @@ std::wstring GetProcessNameFromPid(DWORD pid) {
 
 
 
-void ProcessVolumes(IAudioSessionEnumerator* enumerator) {
+void handleVolumes(IAudioSessionEnumerator* enumerator) {
 	int sessionCount;
 	enumerator->GetCount(&sessionCount);
+	// loop through all session
 	for (int index = 0; index < sessionCount; index++) {
+		// get session
 		IAudioSessionControl* session = nullptr;
 		IAudioSessionControl2* session2 = nullptr;
 		enumerator->GetSession(index, &session);
 		session->QueryInterface(__uuidof(IAudioSessionControl2), (void**)&session2);
 
-		DWORD id = 0;
-		session2->GetProcessId(&id);
+		DWORD pid = 0;
+		session2->GetProcessId(&pid);
 
 		LPWSTR guid;
 		session2->GetSessionInstanceIdentifier(&guid);
 
-		ISimpleAudioVolume* saVol = nullptr;
+		// get process volume
+		ISimpleAudioVolume* simpleAudioVolume = nullptr;
 		float volume;
-		session2->QueryInterface(__uuidof(ISimpleAudioVolume), (void**)&saVol);
-		saVol->GetMasterVolume(&volume);
+		session2->QueryInterface(__uuidof(ISimpleAudioVolume), (void**)&simpleAudioVolume);
+		simpleAudioVolume->GetMasterVolume(&volume);
 
+		// set volume if too loud
 		if (volume >= .7)
 		{
-			std::wstring processName = GetProcessNameFromPid(id);
+			std::wstring processName = getProcessNameFromPid(pid);
 			std::wcout << processName << ": " << volume << " -> " << "0.1" << std::endl;
-			saVol->SetMasterVolume(.1, NULL);
+			simpleAudioVolume->SetMasterVolume(.1, NULL);
 		}
 
 		session2->Release();
@@ -67,12 +71,12 @@ void ProcessVolumes(IAudioSessionEnumerator* enumerator) {
 
 
 
-void backendThread(IAudioSessionEnumerator* enumerator, BOOL* runningFlagPtr)
+void handleVolumeLoop(IAudioSessionEnumerator* enumerator, BOOL* isRunningFlagPtr)
 {
 	std::this_thread::sleep_for(std::chrono::seconds(1));
-	while (*runningFlagPtr)
+	while (*isRunningFlagPtr)
 	{
-		ProcessVolumes(enumerator);
+		handleVolumes(enumerator);
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 }
@@ -88,6 +92,7 @@ int main()
 		MessageBox(NULL, L"COM Initialize Failed!", L"Error", MB_ICONERROR);
 		return 1;
 	}
+
 	// create MMDeviceEnumerator instance
 	IMMDeviceEnumerator* deviceEnumerator = nullptr;
 	hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_INPROC_SERVER, __uuidof(IMMDeviceEnumerator), (LPVOID*)&deviceEnumerator);
@@ -96,27 +101,28 @@ int main()
 		MessageBox(NULL, L"Create MMDeviceEnumerator Instance Failed!", L"Error", MB_ICONERROR);
 		return 1;
 	}
+
 	// get default audio device
 	IMMDevice* device = nullptr;
 	deviceEnumerator->GetDefaultAudioEndpoint(eRender, eMultimedia, &device);
 	
+	// get audio session manager
 	IAudioSessionManager2* sessionManager = nullptr;
 	device->Activate(__uuidof(IAudioSessionManager2), CLSCTX_ALL, nullptr, (void**)&sessionManager);
 
+	// get session enum
 	IAudioSessionEnumerator* enumerator = nullptr;
 	sessionManager->GetSessionEnumerator(&enumerator);
 
-
-	// start backend thread
-	BOOL runningFlag = TRUE;
-	std::thread backend(backendThread, enumerator, &runningFlag);
+	// start handle volume thread
+	BOOL isRunning = TRUE;
+	std::thread handleVolumeThread(handleVolumeLoop, enumerator, &isRunning);
 
 	// user input loop
 	std::string userInput;
 	while (1)
 	{
 		std::cin >> userInput;
-		std::cout << "User Input: " << userInput << std::endl;
 		if (userInput.compare("quit") == 0)
 		{
 			std::cout << "Quit App" << std::endl;
@@ -130,8 +136,8 @@ int main()
 	}
 
 	// wait backend thread stop
-	runningFlag = FALSE;
-	backend.join();
+	isRunning = FALSE;
+	handleVolumeThread.join();
 
 	// release 
 	deviceEnumerator->Release();
